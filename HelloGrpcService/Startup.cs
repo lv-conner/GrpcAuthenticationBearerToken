e0e6909f30e3e4ed48255cf9e43049b30744a8b9
lv-conner <lvmh123456@gmail.com>
2019-09-24 10:22:37 +0800

    添加项目文件。

---
+++ HelloGrpcService/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloGrpcService
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddGrpc();
+            services.AddAuthentication("Bearer").AddJwtBearer("Bearer",options =>
+            {
+                options.Authority = "http://localhost:5000";
+                options.RequireHttpsMetadata = false;
+                options.Audience = "api1";
+                options.TokenValidationParameters.ValidIssuer = "http://localhost:5000";
+                options.TokenValidationParameters.ValidateAudience = true;
+                options.TokenValidationParameters.ValidateIssuerSigningKey = false;
+                options.TokenValidationParameters.ValidateAudience = true;
+                options.TokenValidationParameters.ValidateIssuer = false;
+            });
+            services.AddAuthorization();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
---
+++ HelloGrpcServiceClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Net.Client;
+using HelloGrpcService;
+using System;
+using System.Net.Http;
+using IdentityModel;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace HelloGrpcServiceClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // request token
+            var tokenClient = new TokenClient(new HttpClient() { BaseAddress = new Uri("http://localhost:5000/connect/token") }, new TokenClientOptions()
+            {
+                ClientId = "client",
+                ClientSecret = "secret"
+            });
+            var tokenResponse = await tokenClient.RequestClientCredentialsTokenAsync();
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+            Console.WriteLine(tokenResponse.Json);
+
+            var httpClient = new HttpClient();
+            httpClient.SetBearerToken(tokenResponse.AccessToken);
+            var channel = GrpcChannel.ForAddress("https://localhost:8888", new GrpcChannelOptions()
+            {
+                HttpClient = httpClient
+            }); 
+            var client = new Greeter.GreeterClient(channel);
+            var helloReply = await client.SayHelloAsync(new HelloRequest() { Name = "tim lv" });
+            Console.WriteLine(helloReply.Message);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
